46c09cf6098ef49f73cd1371cf7b0793cbe24be4
TARS30 <chernovalexandr30@gmail.com>
2023-07-09 13:35:55 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+export default function App() {
+  const [amount, setAmount] = useState('1');
+  const [toCur, setToCur] = useState("EUR");
+  const [fromCur, setFromCurr] = useState("USD");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function convert() {
+      if (amount === 0) {
+        return
+      }
+      if (fromCur === toCur) {
+        return setConverted(amount)
+      }
+      setIsLoading(true);
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+      );
+      const data = await res.json();
+      setConverted(data.rates[toCur]);
+    }
+    convert();
+    setIsLoading(false);
+  }, [amount, fromCur, toCur]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="centered">
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+        placeholder="Enter Amount"
+      />
+      <div className="selects">
+        <select value={fromCur} onChange={(e) => setFromCurr(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <p>👉</p>
+        <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+      {!isLoading && (
+        <p className="converted-value">
+          {converted} <span>{toCur}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
